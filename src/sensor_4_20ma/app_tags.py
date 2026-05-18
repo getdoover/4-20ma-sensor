e17907416b26d98b333127d6749cd1ace9abc271
mathsman5133 <mathsman5132@gmail.com>
2026-05-18 14:42:24 +1000

    add live tags

--- src/sensor_4_20ma/app_tags.py
+++ src/sensor_4_20ma/app_tags.py
@@ -2,7 +2,7 @@ from pydoover.tags import Tag, Tags
 
 
 class Sensor420maTags(Tags):
-    value = Tag("number", default=None)
+    value = Tag("number", default=None, live=True)
     unfiltered_value = Tag("number", default=None)
     raw_value = Tag("number", default=None)
     polling_frequency = Tag("number", default=None)
